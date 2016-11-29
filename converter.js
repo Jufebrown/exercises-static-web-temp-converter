fac24489e03255ed4d6c9535a69d7f86851b8519
Jufebrown <jufebrown@gmail.com>
2016-11-29 13:52:51 -0600

    completed determineConverter code

--- converter.js
+++ converter.js
@@ -15,15 +15,22 @@ var button = document.getElementById("converter");
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
+	console.log("event", clickEvent);
+	var inputNum = document.getElementById("tempNum").value;
+	var tempType = document.getElementsByName("tempScale").value;
+  	if(tempType === "fahrenheit") {
+  		toFahrenheit(inputNum);
+  	} else {
+  		toCelsius(inputNum);
+  	}
 }
 
 // Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+button.addEventListener("click", determineConverter());
 
 // clear input field with clear button
 var clearButton = document.getElementById("clear");
 function clearInput () {
 	document.getElementById("tempNum").value = "";
 }
-clearButton.addEventListener("click", clearInput())
+clearButton.addEventListener("click", clearInput());
