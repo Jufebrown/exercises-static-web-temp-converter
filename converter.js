5da71f328889dc2963349163ad0968c37425526f
Jufebrown <jufebrown@gmail.com>
2016-11-29 15:40:52 -0600

    donezo

--- converter.js
+++ converter.js
@@ -1,11 +1,42 @@
 
+var convertedTemp = 0;
+var fUnit = " &#8457;";
+var cUnit = " &#8451;";
+var unit = "";
+document.getElementById("fahrenheit").checked
 
+function outputText () {
+	document.getElementById("tempOutput").innerHTML = convertedTemp + unit;
+	colorText();
+} 
 
-function toCelsius () {
-
+function toCelsius (temp) {
+	convertedTemp = ((temp - 32) * (5/9));
+	unit = cUnit;
+	outputText();
 }
 
-function toFahrenheit () {
+function toFahrenheit (temp) {
+	convertedTemp = (temp * (9/5) + 32);
+	unit = fUnit;
+	outputText();
+}
+
+// Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
+
+// If the temperature is less than 32F/0C the color of the converted temperature should be blue.
+// For any other temperature, the color should be green.
+
+function colorText () {
+// If the temperature is greater than 90F/32C the color of the converted temperature should be red.
+	if ((document.getElementById("fahrenheit").checked && convertedTemp > 32) || ((document.getElementById("fahrenheit").checked === false) && (convertedTemp > 90))) {
+		document.getElementById("tempOutput").style.color = "red";
+	} else if ((document.getElementById("fahrenheit").checked && convertedTemp < 0) || ((document.getElementById("fahrenheit").checked === false) && (convertedTemp < 32))) {
+		document.getElementById("tempOutput").style.color = "blue";
+	} else {
+		document.getElementById("tempOutput").style.color = "green";
+	}
+
 
 }
 
@@ -15,22 +46,24 @@ var button = document.getElementById("converter");
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
+	event.preventDefault();
 	console.log("event", clickEvent);
 	var inputNum = document.getElementById("tempNum").value;
-	var tempType = document.getElementsByName("tempScale").value;
-  	if(tempType === "fahrenheit") {
-  		toFahrenheit(inputNum);
-  	} else {
+	console.log(document.getElementById("fahrenheit").checked);
+  	if(document.getElementById("fahrenheit").checked === true) {
   		toCelsius(inputNum);
+  	} else {
+  		toFahrenheit(inputNum);
   	}
 }
 
 // Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter());
+button.addEventListener("click", determineConverter);
 
 // clear input field with clear button
 var clearButton = document.getElementById("clear");
 function clearInput () {
 	document.getElementById("tempNum").value = "";
 }
-clearButton.addEventListener("click", clearInput());
+clearButton.addEventListener("click", clearInput);
+
