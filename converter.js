bd6b5e98d62c078bbc6b55152d54a02996f46864
Jufebrown <jufebrown@gmail.com>
2017-04-06 16:01:17 -0500

    changed to es6. added comments

--- converter.js
+++ converter.js
@@ -21,54 +21,56 @@ const toCelsius = (temp) => {
 	outputText();
 }
 
-function toFahrenheit (temp) {
+// converts to F
+const toFahrenheit = (temp) => {
 	convertedTemp = (temp * (9/5) + 32);
 	unit = fUnit;
 	outputText();
 }
 
-// Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
 
-// If the temperature is less than 32F/0C the color of the converted temperature should be blue.
-// For any other temperature, the color should be green.
 
-function colorText () {
+const colorText = () => {
 // If the temperature is greater than 90F/32C the color of the converted temperature should be red.
 	if ((document.getElementById("fahrenheit").checked && convertedTemp > 32) || ((document.getElementById("fahrenheit").checked === false) && (convertedTemp > 90))) {
 		document.getElementById("tempOutput").style.color = "red";
+// If the temperature is less than 32F/0C the color of the converted temperature should be blue.
 	} else if ((document.getElementById("fahrenheit").checked && convertedTemp < 0) || ((document.getElementById("fahrenheit").checked === false) && (convertedTemp < 32))) {
 		document.getElementById("tempOutput").style.color = "blue";
+// For any other temperature, the color should be green.
 	} else {
 		document.getElementById("tempOutput").style.color = "green";
 	}
-
-
 }
 
 // Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
+const button = document.getElementById("converter");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
-function determineConverter (clickEvent) {
+const determineConverter = (clickEvent) => {
 	event.preventDefault();
-	console.log("event", clickEvent);
-	var inputNum = document.getElementById("tempNum").value;
+	// console.log("event", clickEvent);
+	let inputNum = document.getElementById("tempNum").value;
 	console.log(document.getElementById("fahrenheit").checked);
-  	if(document.getElementById("fahrenheit").checked === true) {
-  		toCelsius(inputNum);
-  	} else {
-  		toFahrenheit(inputNum);
-  	}
+	if(document.getElementById("fahrenheit").checked === true) {
+		toCelsius(inputNum);
+	} else {
+		toFahrenheit(inputNum);
+	}
 }
 
+// Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
 
 // clear input field with clear button
-var clearButton = document.getElementById("clear");
+const clearButton = document.getElementById("clear");
 
-function clearInput () {
+// clears input field
+const clearInput = () => {
 	document.getElementById("tempNum").value = "";
 }
+
+// adds event listener to button
 clearButton.addEventListener("click", clearInput);
